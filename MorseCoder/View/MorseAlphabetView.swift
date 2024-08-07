bf3198d72ee31fd0dcb65ea33fa7123e1937c2dc
Lukas <mogul@seznam.cz>
2024-08-07 13:53:09 +0200

    Another tweaking MorseAlphabetView

--- MorseCoder/View/MorseAlphabetView.swift
+++ MorseCoder/View/MorseAlphabetView.swift
@@ -19,29 +19,74 @@ struct MorseAlphabetView: View {
         let morseLetter = Morse(id: item.key, letter: item.key, code: item.value)
         alphabetLetters.append(morseLetter)
       }
+      if let num = Int(item.key) {
+        let morseNumber = Morse(id: item.key, letter: item.key, code: item.value)
+        alphabetNumbers.append(morseNumber)
+      }
+      if Character(item.key).isSymbol {
+        let morseSymbol = Morse(id: item.key, letter: item.key, code: item.value)
+        alphabetLetters.append(morseSymbol)
+      }
     }
   }
   
-  let columns = [GridItem(.fixed(120), spacing: 20, alignment: .leading), GridItem(.fixed(120), spacing: 20, alignment: .leading)]
+  let letterColumns = [GridItem(.fixed(120), spacing: 10, alignment: .leading), GridItem(.fixed(120), spacing: 10, alignment: .leading)]
+  let numberColumns = [GridItem(.fixed(150))]
   
   var body: some View {
     ScrollView(showsIndicators: false) {
-      LazyVGrid(columns: columns, spacing: 10) {
+      LazyVGrid(columns: letterColumns, spacing: 10) {
         ForEach(alphabetLetters.sorted(by: { $0.letter < $1.letter })) { item in
-          HStack {
-            Text(item.letter)
-            Text(item.code)
+          HStack(spacing: 0) {
+            Text(item.letter).frame(width: 20)
+            Text(":").frame(width: 20)
+            setBaselineOffsetToDot(item.code)
+              .frame(width: 60, alignment: .center)
+              .font(.system(size: 36, weight: .bold))
           }
-          .font(.system(size: 30, weight: .bold))
-          .padding()
-          .frame(width: 120)
+          .font(.system(size: 24, weight: .bold))
+          .padding(.horizontal, 12)
+          .frame(width: 120, alignment: .leading)
         }
-        .background(Color.yellow)
+        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2))
+      }
+      
+      Divider()
+      
+      LazyVGrid(columns: numberColumns, spacing: 10) {
+        ForEach(alphabetNumbers.sorted(by: { $0.letter < $1.letter })) { item in
+          HStack(spacing: 0) {
+            Text(item.letter).frame(width: 20)
+            Text(":").frame(width: 20)
+            setBaselineOffsetToDot(item.code)
+              .frame(width: 100, alignment: .center)
+              .font(.system(size: 36, weight: .bold))
+          }
+          .font(.system(size: 24, weight: .bold))
+          .padding(.horizontal, 12)
+          .frame(width: 160, alignment: .leading)
+        }
+        .background(RoundedRectangle(cornerRadius: 10).stroke(Color.gray, lineWidth: 2))
       }
     }
     .navigationTitle("Morse Alphabet")
     .navigationBarTitleDisplayMode(.inline)
   }
+  
+  private func setBaselineOffsetToDot(_ code: String) -> Text {
+    var text = Text("")
+    
+    for char in code {
+      if char == "." {
+        text = text + Text("\(char)").baselineOffset(13)
+      } else {
+        text = text + Text("\(char)").baselineOffset(5)
+      }
+    }
+    
+    return text
+  }
+  
 }
 
 #Preview {

d1dbc1652ed089cded79c8b126fc58e64e1444ab
Lukas <mogul@seznam.cz>
2024-08-09 09:39:38 +0200

    Adding dismiss private

--- MorseCoder/View/MorseAlphabetView.swift
+++ MorseCoder/View/MorseAlphabetView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct MorseAlphabetView: View {
   
-  @Environment(\.dismiss) var dismiss
+  @Environment(\.dismiss) private var dismiss
   
   var alphabetLetters: [Morse] = []
   var alphabetNumbers: [Morse] = []
